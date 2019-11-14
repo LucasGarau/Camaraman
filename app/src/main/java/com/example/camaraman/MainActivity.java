276f6e9d2d147841fa59eefc744d5c24308e9dee
LucasGarau <Lucas1012000>
2019-11-14 17:28:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/camaraman/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.camaraman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.hello);
+    }
+
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            //Androidx.core.contentprovider
+        }
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+}
